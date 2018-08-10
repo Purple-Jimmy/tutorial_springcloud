4bdf21630ca859b06d5be67d74022d9ba2a5041b
jimmy <773792867@qq.com>
2018-08-10 18:23:21 +0800

    spingcloud

--- tutorial_cloud_consul/src/main/java/com/cloud/RegisterCenterStart.java
+++ tutorial_cloud_consul/src/main/java/com/cloud/RegisterCenterStart.java
@@ -3,15 +3,35 @@ package com.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: jimmy
  * @Date: 2018/8/9
+ * https://blog.csdn.net/zjcjava/article/details/78608892
+ * https://yq.aliyun.com/articles/334533
+ * https://blog.csdn.net/xp_lx1/article/details/81195768
+ * https://blog.csdn.net/valada/article/details/80878737
+ * http://soft.dog/2016/03/19/consul-cluster/
+ * https://blog.csdn.net/ityouknow/article/details/81125446
+ * http://www.cnblogs.com/sunsky303/p/9209024.html
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class RegisterCenterStart {
     public static void main(String[] args) {
         SpringApplication.run(RegisterCenterStart.class,args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    @RequestMapping("/health")
+    public String health() {
+        return "hello health ";
     }
 }
